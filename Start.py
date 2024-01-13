8a353fe6b240d39b8d93917dde443f15f7d2d70f
courtneyduong <courtneyduong@ucsb.edu>
2024-01-13 14:48:48 -0800

    splits array to divide by individual classes

--- Start.py
+++ Start.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as bs
+import csv
 
 with open("courtneys_schedule.html") as fp:
     soup = bs(fp, "html.parser")
@@ -19,29 +20,55 @@ mondayclasses = soup.find(id = 'pageContent_events')
 
 # all instances of li in classes 'section'
 
-mon = mondayclasses.find_next('li')
-tues2fri = classes.find_next_siblings('li')
+mon_html = mondayclasses.find_next('li')
+tues2fri_html = classes.find_next_siblings('li')
 
-everyclass = tues2fri
-everyclass.insert(0, mon)
+everyclass = tues2fri_html
+everyclass.insert(0, mon_html)
 
 #test if chunk of text is an element in the list -> each day and its classes are an element
 #print(everyclass.pop(0))
 
 classes_each_day = []
+DOW = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
 
 #adds class info (subject, location, start time to end time) to classnames list as strings
 for tag_element in everyclass:
-   p = tag_element.text
-   x = p.split("\n")
-   classes_each_day.append(x)
+   text_element = tag_element.text
+   empty_text_element = text_element.split("\n") # empty meaning not white space
+   classes_each_day.append(empty_text_element)
 
 # classes_each_day is a 2d array
-# removes empty objects
+
 element_to_remove = ""
-for day in classes_each_day:
-    while element_to_remove in day:
-        day.remove(element_to_remove)
+for day_list in classes_each_day:
+    # removes empty objects
+    while element_to_remove in day_list:
+        day_list.remove(element_to_remove)
 
+    for elements in day_list:
+        #removes day of week -> will replace with start day at [1][...]
+        for day in DOW:
+            if day in elements:
+                day_list.remove(day)
+        #
 
-print(classes_each_day)
+modified_array = [
+    [subarray[i:i+3] for i in range(0, len(subarray), 3)]
+    for subarray in classes_each_day
+]
+
+# Print the modified array
+for subarray in modified_array:
+    print(subarray)
+    
+
+#print(classes_each_day)
+
+#parse info into csv file
+csv_file_path = 'Schedule.csv'
+
+with open(csv_file_path, "w", newline='', encoding = 'utf-8') as csv_file:
+
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerows(subarray)
