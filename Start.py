520b23af8fdb2aae31e9505728ba97f3ea6e38c4
Yarwin Liu <yarwinliu@ucsb.edu>
2024-01-12 22:03:11 -0800

    prints out html file

--- Start.py
+++ Start.py
@@ -1,3 +1,14 @@
 from bs4 import BeautifulSoup as bs
 
+with open("schedule_yarwin.html") as fp:
+    soup = bs(fp, 'html.parser')
+
+#soup = bs("<html>a web page</html>", 'html.parser')
+
+#print(bs("<html><head></head><body>Sacr&eacute; bleu!</body></html>", "html.parser"))
+# <html><head></head><body>Sacré bleu!</body></html>
+
+
+print(soup.prettify())
+
 print("hello world")
