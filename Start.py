ac6716cd1ece7975f318ab447b9b806e29993fd4
courtneyduong <courtneyduong@ucsb.edu>
2024-01-13 10:14:06 -0800

    c

--- Start.py
+++ Start.py
@@ -1,6 +1,23 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 with open("courtneys_schedule.html") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
+    soup = bs(fp, "html.parser")
 
-print(soup.prettify())
+#print(soup.prettify())
+
+"""
+parse subject, start date, start time and end time, location into csv file
+
+- start date: calculated, looped 10 times
+- create 10 google calendar events
+
+"""
+
+monday_classes = soup.find(id ="pageContent_eventsgroupM")
+
+for c in soup.find_all('li'):
+    if c != None:
+        print(c.get('data-content'))
+
+
+#print(monday_classes)
