592b344cbbd8b5e9e4b5827379d345f31f7d3434
courtneyduong <courtneyduong@ucsb.edu>
2024-01-13 18:02:09 -0800

    just need to add start/end dates

--- Start.py
+++ Start.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import csv
+from functions import *
 
 with open("courtneys_schedule.html") as fp:
     soup = bs(fp, "html.parser")
@@ -26,8 +27,7 @@ tues2fri_html = classes.find_next_siblings('li')
 everyclass = tues2fri_html
 everyclass.insert(0, mon_html)
 
-#test if chunk of text is an element in the list -> each day and its classes are an element
-#print(everyclass.pop(0))
+#each day and its classes are an element
 
 classes_each_day = []
 DOW = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
@@ -46,28 +46,38 @@ for day_list in classes_each_day:
     while element_to_remove in day_list:
         day_list.remove(element_to_remove)
 
-    for elements in day_list:
+    for index, elements in enumerate(day_list):
         #removes day of week -> will replace with start day at [1][...]
         for day in DOW:
             if day in elements:
                 day_list.remove(day)
-        #add start and end date, and start and end times b/c they depend on day of week
+        #converts standard time to military time
+        if "AM" in elements or "PM" in elements:
+            day_list[index] = standard_to_military(elements)
+        #split times into start/end times
+        for i in range(len(day_list)):
+            day_list[i:i+1] = day_list[i].split('-')
 
+
+print(classes_each_day)
 # in classes_each_day array, separate each class and its info into its own subarray
 modified_array = [
-    #change to 4s once split time !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    [subarray[i:i+3] for i in range(0, len(subarray), 3)]
+    #change to 8s once all elements r in !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    [subarray[i:i+4] for i in range(0, len(subarray), 4)]
     for subarray in classes_each_day
 ]
 #flatten 3d array to 2d array to display on separate rows for csv file   
 flat_list = [elem for sublist1 in modified_array for elem in sublist1]
 
-#move locations to the end of each subarray for csv formatting
+#rearrange each subarray for csv formatting
 for subarray in flat_list:
+      #fix once start/end times/dates are added
     location = subarray.pop(1)
+    all_day_event = subarray.append(False)
+    description = subarray.append("Description")
     subarray.append(location)
+    private = subarray.append(False)
 
-print(flat_list)
 #parse info into csv file
 csv_file_path = 'Schedule.csv'
 
---
+++ functions.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+def date_calculator(week_number):
+    """
+    returns list depending on param
+    """
+    month_dic = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    }
+
+    month = 1
+    day = 8 + week_number * 7
+    year = 24
+
+    if day >= month_dic[month]:
+        month += 1
+        p = month
+        while p > 0:
+            day = day - month_dic[p]
+            p-=1
+
+    if day <= 10:
+        sday = "0"+ str(day)
+    else:
+        sday = str(day)
+    if month <= 10:
+        smonth = "0"+ str(month)
+    else:
+        smonth = str(month)
+
+    if month >12:
+        year += 1
+        syear = str(year)
+    else:
+        syear = str(year)
+
+    date = smonth + "/" + sday + "/" + syear
+
+    return date
+
+
+def standard_to_military(time_str):
+    dt_object = datetime.strptime(time_str, "%I:%M %p")
+    military_time = dt_object.strftime("%H:%M")
+
+    return military_time
+
+
+print(standard_to_military("01:15 PM"))
